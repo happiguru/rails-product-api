f3c6bf308a9aed9077af37e05ce564efa9b8b462
happiguru <hguruman@gmail.com>
2021-09-10 15:37:18 +0400

    Fix error message

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,29 +1,29 @@
 class Api::V1::ProductsController < ApplicationController
   def index
     products = Product.all
-    render json: products, status: :200
+    render json: JSON.pretty_generate(JSON.parse(products.to_json))
   end
 
   def create
     product = Product.new(
       name: prod_params[:name],
       brand: prod_params[:brand],
-      price: prod_params[:price]
+      price: prod_params[:price],
       description: prod_params[:description]
     )
     if product.save
-      render json: products, status: 200
+      render json: JSON.pretty_generate(JSON.parse(products.to_json))
     else
-      render json: {error: "Error creating review."}
+      render json: JSON.pretty_generate({error: "Error creating review."})
     end
   end
 
   def show
     product = Product.find_by(id: params[:id])
     if product
-      render json: product, status: :200
+      render json: JSON.pretty_generate(JSON.parse(product.to_json)), status: 200
     else
-      render json: {error: "Product not found"}
+      render json: JSON.pretty_generate({error: "Product not found"})
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+    {
+        name: "Quilet Comfort 35",
+        brand: "Bose",
+        price: "$279.00",
+        description: "Wireless BT"
+    },
+    {
+        name: "Zero One",
+        brand: "Cat",
+        price: "$2379.00",
+        description: "Zumbie Man Wireless BT"
+    },
+    {
+        name: "Info DOt",
+        brand: "Dot",
+        price: "$5279.00",
+        description: "My wife Wireless BT"
+    },
+    {
+        name: "Guillet",
+        brand: "Guil",
+        price: "$2793.00",
+        description: "Zero Number Wireless BT"
+    },
+    {
+        name: "Chocolate",
+        brand: "Bose",
+        price: "$279.00",
+        description: "Wireless BT"
+    }
+])
